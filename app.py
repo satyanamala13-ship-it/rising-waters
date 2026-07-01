d3c1cfc360b321e946c4fe1885f2005a0c820e26
Namala Satya sri <satyanamala13@gmail.com>
2026-07-01 11:51:21 +0530

    updated Flask app

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect
+import joblib
+import numpy as np
 app = Flask(__name__)
+model=joblib.load("model.pkl")
 @app.route("/")
 def home():
-    return "Flood Prediction App is Running Successfully!"
+    return render_template("index.html")
+@app.route("/predict", methods=["POST"])
+def predict():
+    temp = float(request.form["temp"])
+    humidity = float(request.form["humidity"])
+    cloud = float(request.form["cloud"])
+    prediction=model.predict([[temp,humidity,cloud]])
+    if prediction[0]==1:
+        return redirect("/chance")
+    else:
+        return redirect("/no-chance")
+@app.route("/chance")
+def chance():
+    return render_template("chance.html")
+@app.route("/no-chance")
+def no_chance():
+    return render_template("no_chance.html")
 if __name__ == "__main__":
     app.run(debug=True)
